e553685d5a2d94d949175377407f00d95069ed92
siroron <nu0sgtete@gmail.com>
2025-03-11 22:24:25 +0900

    tailwindcssをsrc直下以外でも適応できるようにした

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/***.jsx",
-    "./src/***.js",
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html"
   ],
   theme: {
     extend: {},
