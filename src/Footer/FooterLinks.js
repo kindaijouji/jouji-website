8b4b03bedbd104c01b76a5b0440202d4791d3a15
siroron <nu0sgtete@gmail.com>
2025-03-15 02:44:18 +0900

    footerのリンクを繋いだ

--- src/Footer/FooterLinks.js
+++ src/Footer/FooterLinks.js
@@ -8,9 +8,9 @@ const FooterLinks = () => {
         <div>
           <h3 className="text-sm font-bold mb-3">ABOUT</h3>
           <ul className="space-y-2">
-            <li><a href="/contact" className="text-gray-400 hover:text-white no-underline transition-colors">自治会について</a></li>
-            <li><a href="/contact" className="text-gray-400 hover:text-white no-underline transition-colors">年間予定</a></li>
-            <li><a href="/contact" className="text-gray-400 hover:text-white no-underline transition-colors">過去イベント一覧</a></li>
+            <li><a href="/#about/#main-visual" className="text-gray-400 hover:text-white no-underline transition-colors">自治会について</a></li>
+            <li><a href="/#about/#annual-event" className="text-gray-400 hover:text-white no-underline transition-colors">年間予定</a></li>
+            <li><a href="/#about/#past-events" className="text-gray-400 hover:text-white no-underline transition-colors">過去イベント一覧</a></li>
           </ul>
         </div>
 
@@ -18,8 +18,8 @@ const FooterLinks = () => {
         <div>
           <h3 className="text-sm font-bold mb-3">情報学部祭</h3>
           <ul className="space-y-2">
-            <li><a href="/contact" className="text-gray-400 hover:text-white no-underline transition-colors">情報学部祭とは</a></li>
-            <li><a href="/contact" className="text-gray-400 hover:text-white no-underline transition-colors">過去の企画</a></li>
+            <li><a href="/#KdixFes/#fes-info" className="text-gray-400 hover:text-white no-underline transition-colors">情報学部祭とは</a></li>
+            <li><a href="/#KdixFes/#past-fes" className="text-gray-400 hover:text-white no-underline transition-colors">過去の企画</a></li>
           </ul>
         </div>
 
@@ -27,8 +27,8 @@ const FooterLinks = () => {
         <div>
           <h3 className="text-sm font-bold mb-3">質問箱</h3>
           <ul className="space-y-2">
-            <li><a href="/contact" className="text-gray-400 hover:text-white no-underline transition-colors">質問フォーム</a></li>
-            <li><a href="/contact" className="text-gray-400 hover:text-white no-underline transition-colors">質問と回答</a></li>
+            <li><a href="/#QABox/#qa-form" className="text-gray-400 hover:text-white no-underline transition-colors">質問フォーム</a></li>
+            <li><a href="/#QABox/#qa-list" className="text-gray-400 hover:text-white no-underline transition-colors">質問と回答</a></li>
           </ul>
         </div>
       </div>
