ceb42fde1175c4426e69edb559edde451a9b5a93
siroron <nu0sgtete@gmail.com>
2025-06-09 17:07:19 +0900

    Revert "Homeから学部長会談アンケートを削除"
    This reverts commit cc9dcdcede5218590e1f33ccc45532f3e8453274.

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -2,13 +2,13 @@ import React from 'react';
 // import HomeHeroSection from './HomeHeroSection';
 import HomeCurrentEvent from './HomeCurrentEvent';
 import HomePastEvent from './HomePastEvent';
-import HomeHeroSection from './HomeHeroSection';
+import GHomeHeroSection from './GHomeHeroSection';
 
 const Home = () => {
   return (
     <div className="min-h-screen">
       {/* ヒーローセクション */}
-      <HomeHeroSection />
+      <GHomeHeroSection />
       
       {/* 現在開催中のイベント */}
       <HomeCurrentEvent />
